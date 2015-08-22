4a04e6477ea4fd1a11c021330bd33bf114ba925b
Firas Wehbe <firaswehbe@users.noreply.github.com>
2015-08-22 14:09:46 -0500

    add vectorisation

---
+++ 09-vectorisation.R
@@ -0,0 +1,11 @@
+# Challenge 3
+
+m <- matrix(1:12, nrow=3, ncol=4)
+m^-1
+m * c(1, 0, -1)
+m > c(0, 20)
+m * c(1, 0, -1, 2)
+
+library(gapminder)
+gapminder[gapminder$country=='Afghanistan',]
+names(gapminder)
